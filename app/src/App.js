71b5fc8f0d8d55fd7d4c89357da96737ab2ef21f
TamikoT <tamikodevo@gmail.com>
2017-08-18 14:49:45 -0700

    Add placeholder pod data, render Node component

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ComponentNode from './component_node'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pods: [1, 2, 3],
+      selectedPod: null
+    };
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Newbernetes</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Kubernetes Visualized
         </p>
+        <div className="component-cluster-div">
+          <ComponentNode
+            onPodSelect={selectedPod => this.setState({selectedPod})}
+            pods={this.state.pods}
+          />
+         </div> 
       </div>
     );
   }
